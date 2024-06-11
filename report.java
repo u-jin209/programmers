e215ba550662a4de24bfa36dce5be7ff5fbed1ce
ujine <ujine@inzent.com>
2024-06-11 16:56:02 +0900

    프로그래머스(신고결과받기 문제 풀이 시간초과)

--- report.java
+++ report.java
@@ -1,24 +1,43 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class report {
 
-    public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-
-        Map<String, Integer> result = new HashMap<>();
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        Map<String, Integer>  answer = new HashMap<>();
+        Map<String, Object> result = new HashMap<>();
+        int[] values = new int[id_list.length];
         for(String user : id_list){
-            result.put(user,0);
+            List<String> reportUser = new ArrayList<>();
+            for (int i= 0; i< report.length; i++){
+                String[] list = report[i].split(" ");
+                if(list[1].equals(user) && !reportUser.contains(list[0]) ){
+                    reportUser.add(list[0]);
+                }
+            }
+            result.put(user,reportUser);
+            answer.put(user,0);
         }
 
-        for (String rep : report){
-            String[] list = rep.split(" ");
-
+        for(String s : result.keySet()){
+            List<String> re = (List<String>) result.get(s);
+            if(re.size() >= k){
+                for (String who : re){
+                    answer.put(who,answer.get(who)+1);
+                }
+            }
         }
-        return answer;
+        int[] end = new int[id_list.length];
+        for (int i=0; i<end.length;i++){
+            end[i] = answer.get(id_list[i]);
+        }
+
+        return end;
     }
 
     public static void main(String[] args) {
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        System.out.println(Arrays.toString(solution(id_list, report, 2)));
 
     }
 }
