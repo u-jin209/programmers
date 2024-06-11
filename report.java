dd8260ca58b5dd35714b458a125e5031ac4465a3
ujine <ujine@inzent.com>
2024-06-11 17:15:51 +0900

    프로그래머스(신고결과받기 문제 풀이 시간초과 6개)

--- report.java
+++ report.java
@@ -4,8 +4,9 @@ public class report {
 
     public static int[] solution(String[] id_list, String[] report, int k) {
         Map<String, Integer>  answer = new HashMap<>();
-        Map<String, Object> result = new HashMap<>();
-        int[] values = new int[id_list.length];
+        for(String user : id_list){
+            answer.put(user,0);
+        }
         for(String user : id_list){
             List<String> reportUser = new ArrayList<>();
             for (int i= 0; i< report.length; i++){
@@ -14,19 +15,17 @@ public class report {
                     reportUser.add(list[0]);
                 }
             }
-            result.put(user,reportUser);
-            answer.put(user,0);
-        }
 
-        for(String s : result.keySet()){
-            List<String> re = (List<String>) result.get(s);
-            if(re.size() >= k){
-                for (String who : re){
+            if(reportUser.size() >= k){
+                for (String who : reportUser){
                     answer.put(who,answer.get(who)+1);
                 }
             }
+
         }
         int[] end = new int[id_list.length];
+
+
         for (int i=0; i<end.length;i++){
             end[i] = answer.get(id_list[i]);
         }
