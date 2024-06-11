7b3a0b10ffb3f32014b90f8899c5054e2763c5a6
ujine <ujine@inzent.com>
2024-06-11 17:58:54 +0900

    프로그래머스(신고결과받기 문제 풀이 시간초과 6개) - ing

--- report.java
+++ report.java
@@ -5,9 +5,6 @@ public class report {
     public static int[] solution(String[] id_list, String[] report, int k) {
         Map<String, Integer>  answer = new HashMap<>();
         for(String user : id_list){
-            answer.put(user,0);
-        }
-        for(String user : id_list){
             List<String> reportUser = new ArrayList<>();
             for (int i= 0; i< report.length; i++){
                 String[] list = report[i].split(" ");
@@ -15,19 +12,23 @@ public class report {
                     reportUser.add(list[0]);
                 }
             }
-
             if(reportUser.size() >= k){
                 for (String who : reportUser){
-                    answer.put(who,answer.get(who)+1);
+                    if(!answer.containsKey(who)){
+                        answer.put(who,1);
+                    }else{
+                        answer.put(who,answer.get(who)+1);
+                    }
                 }
             }
-
         }
         int[] end = new int[id_list.length];
 
 
         for (int i=0; i<end.length;i++){
-            end[i] = answer.get(id_list[i]);
+            if(answer.get(id_list[i]) != null){
+                end[i] = answer.get(id_list[i]);
+            }else {end[i] =0;}
         }
 
         return end;
