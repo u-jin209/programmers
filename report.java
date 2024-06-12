5a4721375a9116afe64af97f0c174593e28e5469
ujine <ujine@inzent.com>
2024-06-12 09:20:49 +0900

    프로그래머스(신고결과받기 문제 풀이 시간초과 5개) - ing

--- report.java
+++ report.java
@@ -5,31 +5,30 @@ public class report {
 
     public static int[] solution(String[] id_list, String[] report, int k) {
         int[] end = new int[id_list.length];
-        Map<String, Object> total = new HashMap<>();
         List<String> arrList =  Arrays.asList(report).stream().distinct().collect(Collectors.toList());
+        List<String> allReportId = new ArrayList<>();
+        int idx =0;
         for(String id : id_list){
             System.out.println("id : " +id);
-            Map<String ,Object> reportTotal = new HashMap<>();
-            int[] count = {0,0};
             List<String> reportId = new ArrayList<>();
             for(String r: arrList){
                 String[] rep = r.split(" ");
-                if(rep[0].equals(id)){
-                    count[0]++;
-                }else if(rep[1].equals(id)){
-                    count[1]++;
+                if(rep[1].equals(id)){
                     reportId.add(rep[0]);
                 }
             }
-            reportTotal.put("reportId",reportId);
-            reportTotal.put("count",count);
 
-            total.put(id, reportTotal);
+            if(reportId.size()>=k){
+                 allReportId.addAll(reportId);
+            }
         }
-
-        for (int i=0; i<total.size();i++){
-            total.get(1);
-            System.out.println("total"+ total.get(i));
+        for (String i : id_list){
+            for(String all : allReportId){
+                if(i.equals(all)){
+                    end[idx]++;
+                }
+            }
+            idx++;
         }
 
         return end;
