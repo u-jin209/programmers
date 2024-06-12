fa36f6fe9d616e92a92353ae84b52bb2cb8ee07e
ujine <ujine@inzent.com>
2024-06-12 13:19:54 +0900

    프로그래머스(신고결과받기 문제 풀이 완료)

--- report.java
+++ report.java
@@ -6,31 +6,37 @@ public class report {
     public static int[] solution(String[] id_list, String[] report, int k) {
         int[] end = new int[id_list.length];
         List<String> arrList =  Arrays.asList(report).stream().distinct().collect(Collectors.toList());
-        List<String> allReportId = new ArrayList<>();
+        Map<String, Object> reportList = new HashMap<>();
+        Map<String, Integer> stopId = new HashMap<>();
+        Map<String, Integer> idIdx= new HashMap<>();
+
         int idx =0;
         for(String id : id_list){
-            System.out.println("id : " +id);
-            List<String> reportId = new ArrayList<>();
-            for(String r: arrList){
-                String[] rep = r.split(" ");
-                if(rep[1].equals(id)){
-                    reportId.add(rep[0]);
-                }
-            }
-
-            if(reportId.size()>=k){
-                 allReportId.addAll(reportId);
-            }
-        }
-        for (String i : id_list){
-            for(String all : allReportId){
-                if(i.equals(all)){
-                    end[idx]++;
-                }
-            }
+            idIdx.put(id,idx);
+            reportList.put(id, new ArrayList<>());
             idx++;
         }
+        for(String r: arrList){
+            String[] rep = r.split(" ");
+            if(stopId.containsKey(rep[1])){
+                stopId.put(rep[1], stopId.get(rep[1])+1);
 
+            }else{
+                stopId.put(rep[1], 1);
+            }
+            List<String> user = (List<String>) reportList.get(rep[1]);
+            user.add(rep[0]);
+            reportList.put(rep[1], user);
+        }
+        for(String s : reportList.keySet()){
+            List<String> rep = (List<String>) reportList.get(s);
+            if(rep.size() >= k){
+                for(String r : rep){
+                    end[idIdx.get(r)]++;
+
+                }
+            }
+        }
         return end;
     }
 
