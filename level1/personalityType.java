5a7702a4843e33f6631fe1c2adb4f05839d7daf2
ujine <ujine@inzent.com>
2024-06-13 10:57:17 +0900

    프로그래머스(성격유형검사하기 풀이 완료)

--- level1/personalityType.java
+++ level1/personalityType.java
@@ -4,24 +4,53 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class personalityType {
-    public static int[] solution(String[] survey, int[] choices) {
-        int[] end = new int[1];
+    public static String solution(String[] survey, int[] choices) {
+        String result ="";
         Map<Integer, String[]> type= new HashMap<>();
-        type.put(1, new String[]{"R", "T"});
-        type.put(2, new String[]{"C", "F"});
-        type.put(3, new String[]{"J", "M"});
-        type.put(4, new String[]{"A", "N"});
+        type.put(0, new String[]{"R", "T"});
+        type.put(1, new String[]{"C", "F"});
+        type.put(2, new String[]{"J", "M"});
+        type.put(3, new String[]{"A", "N"});
 
+        Map<String, Integer> testResult = new HashMap<>();
+        for(int i=0; i<survey.length;i++){
+            String[] sur = survey[i].split("");
 
-        return end;
+            if(choices[i]<4){
+                if(testResult.containsKey(sur[0])){
+                    testResult.put(sur[0], testResult.get(sur[0])+(4-choices[i]));
+                }else{
+                    testResult.put(sur[0], 4-choices[i]);
+                }
+            } else if (choices[i] > 4) {
+                if(testResult.containsKey(sur[1])) {
+                    testResult.put(sur[1], testResult.get(sur[1]) + (choices[i] - 4));
+                }else{
+                    testResult.put(sur[1], choices[i] - 4);
+                }
+            }
+        }
+        for(int i=0; i< type.size();i++){
+            String[] typeList =type.get(i);
+            if(testResult.containsKey(typeList[0]) && testResult.containsKey(typeList[1])){
+                if(testResult.get(typeList[0]) >= testResult.get(typeList[1])){
+                    result+=typeList[0];
+                }else if (testResult.get(typeList[0]) < testResult.get(typeList[1])){
+                    result+=typeList[1];
+                }
+            }else if(testResult.containsKey(typeList[0]) && !testResult.containsKey(typeList[1])){
+                result+=typeList[0];
+            }else if(!testResult.containsKey(typeList[0]) && testResult.containsKey(typeList[1])){
+                result+=typeList[1];
+            }else{
+                result+=typeList[0];
+            }
+
+        }
+        return result;
     }
-    public Map<String, Integer> setScore (){
-        
 
-
-        return new HashMap<>();
-    };
     public static void main(String[] args) {
-        solution(new String[]{"AN", "CF", "MJ", "RT", "NA"}, new int[]{5, 3, 2, 7, 5});
+        solution(new String[]{"TR", "RT", "TR"}, new int[]{7, 1, 3});
     }
 }
